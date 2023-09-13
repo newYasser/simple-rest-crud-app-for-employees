2676d2133914b3c6d0b716c28104daa33a27e98a
newYasser <mohammedyasser0110@gmail.com>
2023-09-13 04:38:25 +0300

    implementing the employeeDAO functions

--- src/main/java/com/example/demo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/example/demo/dao/EmployeeDAOImpl.java
@@ -27,4 +27,28 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
         return employees;
     }
+
+    @Override
+    public Employee findById(int id) {
+
+        Employee employee = entityManager.find(Employee.class,id);
+
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+
+        Employee dbEmployee = entityManager.merge(employee);
+
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+        Employee employee = entityManager.find(Employee.class,id);
+
+        entityManager.remove(employee);
+    }
 }
