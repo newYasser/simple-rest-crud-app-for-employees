5bc6ff90b239677d24c01b53f4ba3a0804f3b9fa
newYasser <mohammedyasser0110@gmail.com>
2023-09-13 03:08:29 +0300

    get all the employees form the DB

---
+++ src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.rest;
+
+import com.example.demo.dao.EmployeeDAO;
+import com.example.demo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
+
+}
